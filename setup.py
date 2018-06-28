2d41a20d9fed9fd9927a6846e82df3579b62531e
Maximilian Bauer <bauer.maximilian@web.de>
2018-06-28 08:01:07 +0200

    Changing also the other files for package name change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cloud-assist",
+    name="cloudassist",
     version="0.0.3",
     author="Maximilian Bauer",
     author_email="bauer.maximilian@web.de",
