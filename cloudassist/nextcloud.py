1db672d4f9f1e53d5e5ea924dcb6793e422db42b
Maximilian Bauer <bauer.maximilian@web.de>
2018-06-29 19:17:47 +0200

    Adjusting decorater style

--- cloudassist/nextcloud.py
+++ cloudassist/nextcloud.py
@@ -121,8 +121,9 @@ class DecoratedOutputStyler(OutputStyler):
 
     def _formatAppointment(self, appointment):
         return_text = ''
-        return_text = return_text + '                                                       _______________\n'
-        return_text = return_text + '  _____________________________________________________|  {}  |_____________________________________________________\n'.format(appointment.startDateTime.strftime("%a %d.%m"))
+        return_text = return_text + '                                                       ┌─────────────┐\n'
+        return_text = return_text + '  ─────────────────────────────────────────────────────┤  {}  ├─────────────────────────────────────────────────────\n'.format(appointment.startDateTime.strftime("%a %d.%m"))
+        return_text = return_text + '                                                       └─────────────┘\n'
         return_text = return_text + '\t\t\t{} - {}\t{}\n'.format(
             appointment.startTime,
             appointment.endTime,
